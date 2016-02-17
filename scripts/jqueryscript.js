4d412943ce849f4da88f8052f9516210a8783ba0
Vigyázó Tom-Tom <gulgulhun@gmail.com>
2016-02-17 20:07:23 +0100

    Navigáció WIP

--- scripts/jqueryscript.js
+++ scripts/jqueryscript.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
+
+  var scount = 0;
+
   function animateLeft() {
+    scount++;
     $(".hatterkep").animate({right: '+=100%'});
+	if (scount == 1){$(".hatterkep").hide(10);}
+	if (scount == 0){$(".hatterkep").show();}
   }
 
   function animateRight() {
+    scount--;
     $(".hatterkep").animate({right: '-=100%'});
+	if (scount == 0){$(".hatterkep").show();}
+    if (scount == -1){$(".hatterkep").hide(10);}
   }
 
   function toggleSlide() {
-    $(".slide_menu").slideToggle(2000);
+    $(".slide_menu").slideToggle("slow");
   }
 
   $(".slide_flip").click(toggleSlide);
